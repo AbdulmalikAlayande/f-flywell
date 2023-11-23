236aac4d9daf84525f199b57ffa79504bcced606
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-11-23 04:42:26 +0100

    comms

--- src/views/components/dashboard/dashboard.tsx
+++ src/views/components/dashboard/dashboard.tsx
@@ -1,32 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import DashBoardSideBar from '../reusableComponents/dashBoardSideBar'
 import DashboardNavBar from '../reusableComponents/dashboardNavBar'
 import "../../../styles/components/dashboard/dashboard.css"
 import { AvailableFlight, CheapFlights, RandomFlights } from '../../interfaces/interface'
-
+import {availableFlight, randomFlight, cheapFlight} from "../../../utilities/placeholder"
 
 const Dashboard = () => {
 
     const [availableFlights, setAvailableFlights] = useState<AvailableFlight[]>([])
     const [cheapFlights, setCheapFlights] = useState<CheapFlights[]>([])
-    const [randomFlights, setRandomFlights] = useState<RandomFlights[]>([])
-    
-    function getRandomInt(min: number, max: number): number {
-        const byteArray = new Uint32Array(1);
-        let unitArray = window.crypto.getRandomValues(byteArray);
-        console.log("unitArray", unitArray)
-        const range = max - min + 1;
-        const maxRange = Math.pow(2, 32) - 1;
-        console.log("maxRange ==> ", maxRange);
-        if (unitArray[0] >= Math.floor(maxRange / range) * range) {
-          return getRandomInt(min, max);
-        }
-        return min + (byteArray[0] % range);
-      }
-      
-    const randomNum = getRandomInt(1, 100);
-    console.log("random number:: ", randomNum);
-      
+    const [randomFlights, setRandomFlights] = useState<RandomFlights[]>([])  
+
+    useEffect(()=>{
+        setAvailableFlights(availableFlight)
+        setRandomFlights(randomFlight)
+        setCheapFlights(cheapFlight)
+    }, [])
   return (
     <div className='Dashboard-Main-Frame'>
         <DashBoardSideBar/>
