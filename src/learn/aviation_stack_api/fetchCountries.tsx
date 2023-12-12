fd5787b3ec0e1429e504d77e7dd29243d2767a89
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-12-12 07:17:05 +0100

    completed the image, web and file part of user profile preference

--- src/learn/aviation_stack_api/fetchCountries.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FetchCountries = () => {
-  return (
-    <div>FetchCountries</div>
-  )
-}
-
-export default FetchCountries
--- src/learn/aviation_stack_api/fetchFlights.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FetchFlights = () => {
-  return (
-    <div>FetchFlights</div>
-  )
-}
-
-export default FetchFlights
