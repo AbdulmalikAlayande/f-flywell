dbbc9dc49794307d21d64354b6a9652d9c521693
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-12-13 22:20:43 +0100

    comms

--- src/views/components/users/dashboard/cheapFlights.tsx
+++ src/views/components/users/dashboard/cheapFlights.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { CheapFlight } from "../../../interfaces/interface";
 import { Icon } from "@iconify/react";
 import "../../../../styles/components/users/dashboard/cheapFlights.css";
+import ButtonWithIcon from "../../reusableComponents/buttonWithIcon";
 
 type Props = {
   cheapFlights: CheapFlight[];
@@ -12,12 +13,23 @@ export function CheapFlights({ cheapFlights }: Props) {
     <div className="Cheap-Flights-Frame">
       {cheapFlights.map((cheapFlight, index) => (
         <div className={"Cheap-Flight"} key={index}>
-          <img src="" alt="From-Location-Ipc" />
-          <p>{cheapFlight.from}</p>
-          <img src="" alt="From-Location-pic" />
-          <p>{cheapFlight.to}</p>
-          <p>{cheapFlight.duration}</p>
-          <p>{cheapFlight.seatsRemaining}</p>
+          <div className="Sect-1">
+            <div className="From-Loc-Frame">
+              <img src="" alt="From-Location-Ipc" />
+              <p>{cheapFlight.from}</p>
+            </div>
+            <div className="To-Loc-Frame">
+              <img src="" alt="To-Location-pic" />
+              <p>{cheapFlight.to}</p>
+            </div>
+            <p>{cheapFlight.duration}</p>
+          </div>
+          <div className="Sect-2">
+            <p>Seats Remaining: {cheapFlight.seatsRemaining}</p>
+            <p>Airline: {"Bola-Air"}</p>
+            <ButtonWithIcon icon={"icon-park-outline:preview-open"}/>
+          </div>
+          
         </div>
       ))}
     </div>
