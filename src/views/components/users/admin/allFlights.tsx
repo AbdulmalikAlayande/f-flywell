bdcb95a763ff780c6950768346020e0621e8cfdb
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-12-29 07:10:30 +0100

    comms

--- src/views/components/users/admin/allFlights.tsx
+++ src/views/components/users/admin/allFlights.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { adminSideBarButtonData } from "../../../../utilities/utility.functions";
+import '../../../../styles/components/users/admin/allFlights.css'
 import DashboardNavBar from "../../reusableComponents/dashboardNavBar";
 import DashBoardSideBar from "../../reusableComponents/dashBoardSideBar";
 
@@ -8,15 +9,15 @@ export default function AllFlights(){
     const buttonDataArray = adminSideBarButtonData();
 
     return(
-        <div>
+        <div className="All-Flights-Main-Frame">
             <DashBoardSideBar userId={""} buttonData={buttonDataArray}/>
-            <div>
+            <div className="All-Flights-Section-2">
                 <DashboardNavBar/>
                 <div className="All-Flights-Main-Body">
                     <section className="All-Flights-Main-Body-Section-1">
                         <h1>Flights</h1>
                         <div className="All-Flights-Main-Body-Filter-Frame">
-                            <p>Filter By</p>
+                            <p>Filter By: </p>
                             <select>
                                 <option>Hello</option>
                                 <option>Hello</option>
