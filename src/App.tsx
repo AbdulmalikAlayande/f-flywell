5d4d00323d5ee5ee5a8e5d08202f9aed49538c31
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-12-31 19:00:12 +0100

    comms

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,25 @@ import AdminSignUp from "./views/components/auth/adminSignUp";
 import Users from "./views/components/users/admin/users/users";
 import AdminDashboard from "./views/components/users/admin/dashboard/adminDashboard";
 import AllFlights from "./views/components/users/admin/allFlights";
+
+
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path={"/:username/dashboard"} element={<Dashboard />} />
-        <Route path={"/home"} element={<LandingPage />} />
-        <Route path={"/"} element={<LandingPage />} />
-        <Route path={"/:username/profile"} element={<Profile />} />
-        <Route path={"/:username/my-trips"} element={<Trips />} />
-        <Route path={"/signup/activate-account"} element={<ActivateUserAccount />}/>
-        <Route path={"/admin-signup"} element={<AdminSignUp />}/>
-        <Route path={"/bola-air/users"} element={<Users />}/>
-        <Route path={"bola-air/admin/dashboard"} element={<AdminDashboard />}/>
-        <Route path={"bola-air/all-flights"} element={<AllFlights />}/>
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path={"/:username/dashboard"} element={<Dashboard />} />
+                <Route path={"/home"} element={<LandingPage />} />
+                <Route path={"/"} element={<LandingPage />} />
+                <Route path={"/:username/profile"} element={<Profile />} />
+                <Route path={"/:username/my-trips"} element={<Trips />} />
+                <Route path={"/signup/activate-account"} element={<ActivateUserAccount />}/>
+                <Route path={"/admin-signup"} element={<AdminSignUp />}/>
+                <Route path={"/bola-air/users"} element={<Users />}/>
+                <Route path={"bola-air/admin/dashboard"} element={<AdminDashboard />}/>
+                <Route path={"bola-air/all-flights"} element={<AllFlights />}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
